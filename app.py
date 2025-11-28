34a947345e7e09cb22b6ada9e45d5fad12260c54
Laion Beltrao <laion_beltrao@hotmail.com>
2025-11-28 10:43:33 -0300

    alteracao no nome das figs e alteracao no use_container_width=True

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ if hist_button:
     st.write('Criando um histograma para o conjunto de dados de anúncio de carros.') # escrever uma mensagem
 
     fig = px.histogram(car_data, x="odometer") # criar histograma
-    st.plotly_chart(fig, width='stretch) # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True) # exibir um gráfico Plotly interativo
 
 if dis_button:
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncio de carros.')
 
     fig1 = px.scatter(car_data, x="odometer", y="price")  # criar um gráfico de dispersão
-    st.plotly_chart(fig1, width='stretch)  # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig1, use_container_width=True)  # exibir um gráfico Plotly interativo
 
 
 # criar uma caixa de seleção
@@ -30,11 +30,11 @@ build_dis = st.checkbox('Criar um gráfico de dispersão')
 if build_histogram: # se a caixa de seleção for selecionada
     st.write('Criando um histograma para a coluna odometer')
 
-    fig = px.histogram(car_data, x="odometer")  # criar histograma
-    st.plotly_chart(fig, width='stretch)  # exibir um gráfico Plotly interativo
+    fig2 = px.histogram(car_data, x="odometer")  # criar histograma
+    st.plotly_chart(fig2, use_container_width=True)  # exibir um gráfico Plotly interativo
 
 if build_dis: # se a caixa de seleção for selecionada
     st.write('Criando um histograma para a coluna odometer')
 
-    fig1 = px.scatter(car_data, x="odometer")  # criar histograma
-    st.plotly_chart(fig1, width='stretch)  # exibir um gráfico Plotly interativo
+    fig3 = px.scatter(car_data, x="odometer", y="price")  # criar histograma
+    st.plotly_chart(fig3, use_container_width=True)  # exibir um gráfico Plotly interativo
